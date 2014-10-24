ffdc73053fb624f92e2fd680808c52a17a534fc1
picturelink <pl@picturelink.net>
2014-10-24 15:02:50 +0100

    Added support for not closing other accordions

--- jquery.accordion.js
+++ jquery.accordion.js
@@ -125,8 +125,13 @@
     }
 
     //hides a accordion panel
-    function close(opts) {
-        opened = $(document).find('.' + opts.cssOpen);
+    function close($this, opts) {
+        if (opts.leaveOpen) {
+            opened = $this;
+        } else {
+            opened = $(document).find('.' + opts.cssOpen);
+        }
+
         $.each(opened, function() {
             //give the proper class to the linked element
             $(this).addClass(opts.cssClose).removeClass(opts.cssOpen);
@@ -136,7 +141,7 @@
 
     //opens a accordion panel
     function open($this, opts) {
-        close(opts);
+        close($this, opts);
         //give the proper class to the linked element
         $this.removeClass(opts.cssClose).addClass(opts.cssOpen);
 
@@ -156,7 +161,7 @@
         // close the only open item
         if ($this.hasClass(opts.cssOpen))
         {
-            close(opts);
+            close($this, opts);
             //do cookies if plugin available
             if (useCookies(opts)) {
                 // split the cookieOpen string by ","
@@ -164,7 +169,7 @@
             }
             return false;
         }
-        close(opts);
+        close($this, opts);
         //open a closed element
         open($this, opts);
         return false;
@@ -268,6 +273,7 @@
             elem.next().hide();
         },
         scrollSpeed: 'slow',  // A string or number determining how long the scroll animation will run.
-        scrollToOpened: false // Flag specifying whether the document should scroll to the opened element
+        scrollToOpened: false, // Flag specifying whether the document should scroll to the opened element
+        leaveOpen: false, // A flag specifying whether open accordions should be left opened when one is closed
     };
 })(jQuery);
